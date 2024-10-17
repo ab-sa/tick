cf066ed17628655793148231d39a3d4ee0e40bd5
Abdollah <a.saffari72@gmail.com>
2024-10-17 13:55:08 +0330

    Update tick_base.py

--- tick_base.py
+++ tick_base.py
@@ -646,6 +646,60 @@ def safe_array(X, dtype=np.float64):
 
     return X
 
+
+
+def extract_dtype(dtype_or_object_with_dtype):
+    import six
+    if (isinstance(dtype_or_object_with_dtype, six.string_types)
+            or isinstance(dtype_or_object_with_dtype, np.dtype)):
+        return np.dtype(dtype_or_object_with_dtype)
+    elif hasattr(dtype_or_object_with_dtype, 'dtype'):
+        return np.dtype(dtype_or_object_with_dtype.dtype)
+    else:
+        raise ValueError("unsupported type used for prox creation, "
+                         "expects dtype or class with dtype , type: {}".format(
+                             dtype_or_object_with_dtype.__class__.__name__))
+
+
+def get_typed_class(clazz, dtype_or_object_with_dtype, dtype_map):
+    clazz.dtype = extract_dtype(dtype_or_object_with_dtype)
+    if np.dtype(clazz.dtype) not in dtype_map:
+        raise ValueError("dtype does not exist in type map for {}".format(
+            clazz.__class__.__name__))
+    return dtype_map[np.dtype(clazz.dtype)]
+
+
+def copy_with(clazz, ignore_fields: list = None):
+    """Copies clazz, temporarily sets values to None to avoid copying.
+       not thread safe
+    """
+    from copy import deepcopy
+
+    if not isinstance(clazz, Base):
+        raise ValueError("Only objects inheriting from Base class should be"
+                         "copied with copy_with.")
+
+    fields = {}
+    for field in ignore_fields:
+        if hasattr(clazz, field) and getattr(clazz, field) is not None:
+            fields[field] = getattr(clazz, field)
+            clazz._set(field, None)
+
+    new_clazz = deepcopy(clazz)
+
+    for field in fields:
+        clazz._set(field, fields[field])
+
+    return new_clazz
+
+
+
+
+
+
+
+
+
 LOSS = "loss"
 GRAD = "grad"
 LOSS_AND_GRAD = "loss_and_grad"
@@ -791,13 +845,18 @@ class Model(ABC, Base):
     def _get_typed_class(self, dtype_or_object_with_dtype, dtype_map):
         """Deduce dtype and return true if C++ _model should be set
         """
-        import tick.base.dtype_to_cpp_type
-        return tick.base.dtype_to_cpp_type.get_typed_class(
-            self, dtype_or_object_with_dtype, dtype_map)
+        #import tick.base.dtype_to_cpp_type
+        #return tick.base.dtype_to_cpp_type.get_typed_class(
+        #    self, dtype_or_object_with_dtype, dtype_map)
+        return get_typed_class(self, dtype_or_object_with_dtype, dtype_map)
 
     def astype(self, dtype_or_object_with_dtype):
-        import tick.base.dtype_to_cpp_type
-        new_model = tick.base.dtype_to_cpp_type.copy_with(
+        #import tick.base.dtype_to_cpp_type
+        #new_model = tick.base.dtype_to_cpp_type.copy_with(
+        #    self,
+        #    ["_model"]  # ignore _model on deepcopy
+        #)
+        new_model = copy_with(
             self,
             ["_model"]  # ignore _model on deepcopy
         )
@@ -1077,18 +1136,24 @@ class Prox(ABC, Base):
         pass
 
     def _get_typed_class(self, dtype_or_object_with_dtype, dtype_map):
-        import tick.base.dtype_to_cpp_type
-        return tick.base.dtype_to_cpp_type.get_typed_class(
-            self, dtype_or_object_with_dtype, dtype_map)
+        #import tick.base.dtype_to_cpp_type
+        #return tick.base.dtype_to_cpp_type.get_typed_class(
+        #    self, dtype_or_object_with_dtype, dtype_map)
+        return get_typed_class(self, dtype_or_object_with_dtype, dtype_map)
 
     def _extract_dtype(self, dtype_or_object_with_dtype):
-        import tick.base.dtype_to_cpp_type
-        return tick.base.dtype_to_cpp_type.extract_dtype(
-            dtype_or_object_with_dtype)
+        #import tick.base.dtype_to_cpp_type
+        #return tick.base.dtype_to_cpp_type.extract_dtype(
+        #    dtype_or_object_with_dtype)
+        return extract_dtype(dtype_or_object_with_dtype)
 
     def astype(self, dtype_or_object_with_dtype):
-        import tick.base.dtype_to_cpp_type
-        new_prox = tick.base.dtype_to_cpp_type.copy_with(
+        #import tick.base.dtype_to_cpp_type
+        #new_prox = tick.base.dtype_to_cpp_type.copy_with(
+        #    self,
+        #    ["_prox"]  # ignore _prox on deepcopy
+        #)
+        new_prox = copy_with(
             self,
             ["_prox"]  # ignore _prox on deepcopy
         )
